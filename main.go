5899b191e0ec34423dce07499b3c97639e0a56b8
paul_lestyo <paulus.lestyo@student.uns.ac.id>
2023-02-08 01:11:37 +0700

    feat: add repository, service, and controller for category

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
+	"github.com/paul-lestyo/belajar-golang-restful-api/app"
+	"github.com/paul-lestyo/belajar-golang-restful-api/controller"
+	"github.com/paul-lestyo/belajar-golang-restful-api/helper"
+	"github.com/paul-lestyo/belajar-golang-restful-api/repository"
+	"github.com/paul-lestyo/belajar-golang-restful-api/service"
+	"net/http"
+)
+
+func main() {
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories/:categoryId", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
